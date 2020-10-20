111007715a459a90267e9a2471f09c5d85ec6937
Peter Williams <pedroeldiablo@yahoo.com>
2020-10-20 15:27:15 +0100

    adding urql as context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
-import { ChakraProvider } from '@chakra-ui/core'
+import {ChakraProvider} from '@chakra-ui/core';
+import {Provider, createClient} from 'urql';
 
 import theme from '../theme'
 import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const client = createClient({url: 'http://localhost:4000/graphql'});
+
+function MyApp({Component, pageProps}: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
+  );
 }
 
 export default MyApp

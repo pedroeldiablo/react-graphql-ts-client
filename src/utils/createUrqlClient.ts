eac3c4505ef23672ba2c6f2053a3784badd56ec4
Peter Williams <pedroeldiablo@yahoo.com>
2020-12-14 22:41:26 +0000

    customising https://github.com/FormidableLabs/urql/blob/main/exchanges/graphcache/src/extras/simplePagination.ts and checking returns at different stages

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -1,6 +1,6 @@
 import {dedupExchange, Exchange, fetchExchange} from 'urql';
 import {pipe, tap} from 'wonka';
-import {cacheExchange} from '@urql/exchange-graphcache';
+import {cacheExchange, Resolver} from '@urql/exchange-graphcache';
 import {
   LogoutMutation,
   MeQuery,
@@ -22,12 +22,85 @@ const errorExchange: Exchange = ({forward}) => (ops$) => {
   );
 };
 
+export const cursorPagination = (
+    cursorArgument = 'cursor',
+): Resolver => {
+  return (_parent, fieldArgs, cache, info) => {
+    const {parentKey: entityKey, fieldName} = info;
+    console.log(entityKey, fieldName);
+    const allFields = cache.inspectFields(entityKey);
+    console.log({allFields});
+    const fieldInfos = allFields.filter((info) => info.fieldName === fieldName);
+    const size = fieldInfos.length;
+    if (size === 0) {
+      return undefined;
+    }
+
+    // const visited = new Set();
+    // let result: NullArray<string> = [];
+    // let prevOffset: number | null = null;
+
+    // for (let i = 0; i < size; i++) {
+    //   const {fieldKey, arguments: args} = fieldInfos[i];
+    //   if (args === null || !compareArgs(fieldArgs, args)) {
+    //     continue;
+    //   }
+
+    //   const links = cache.resolve(entityKey, fieldKey) as string[];
+    //   const currentOffset = args[cursorArgument];
+
+    //   if (
+    //     links === null ||
+    //     links.length === 0 ||
+    //     typeof currentOffset !== 'number'
+    //   ) {
+    //     continue;
+    //   }
+
+    //   const tempResult: NullArray<string> = [];
+
+    //   for (let j = 0; j < links.length; j++) {
+    //     const link = links[j];
+    //     if (visited.has(link)) continue;
+    //     tempResult.push(link);
+    //     visited.add(link);
+    //   }
+
+    //   if (
+    //     (!prevOffset || currentOffset > prevOffset) ===
+    //     (mergeMode === 'after')
+    //   ) {
+    //     result = [...result, ...tempResult];
+    //   } else {
+    //     result = [...tempResult, ...result];
+    //   }
+
+    //   prevOffset = currentOffset;
+    // }
+
+    // const hasCurrentPage = cache.resolve(entityKey, fieldName, fieldArgs);
+    // if (hasCurrentPage) {
+    //   return result;
+    // } else if (!(info as any).store.schema) {
+    //   return undefined;
+    // } else {
+    //   info.partial = true;
+    //   return result;
+    // }
+  };
+};
+
 export const createUrqlClient = (ssrExchange: any) => ({
   url: 'http://localhost:4000/graphql',
   fetchOptions: {
     credentials: 'include' as const,
   },
   exchanges: [dedupExchange, cacheExchange({
+    resolvers: {
+      Query: {
+        posts: cursorPagination(),
+      },
+    },
     updates: {
       Mutation: {
         logout: (_result, _args, cache, _info) => {

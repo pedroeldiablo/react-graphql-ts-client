c86cfcf597d502647038fa3fb797a164e625149d
Peter Williams <pedroeldiablo@yahoo.com>
2020-12-15 00:37:55 +0000

    display paginated results and hide load more if no additional results available

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ import {createUrqlClient} from '../utils/createUrqlClient';
 
 const Index = () => {
   const [variables, setVariables] = useState({
-    limit: 10,
+    limit: 33,
     cursor: null as null | string,
   });
 
@@ -55,7 +55,7 @@ const Index = () => {
             </NextLink> */}
             <br/>
             <Stack spacing={8}>
-              {data!.posts.map((p) => (
+              {data!.posts.posts.map((p) => (
                 <Box key={p.id} p={5} shadow="md" borderWidth="1px">
                   <Heading fontSize="xl">{p.title}</Heading>
                   <Text mt={4}>{p.textSnippet}</Text>
@@ -64,13 +64,13 @@ const Index = () => {
             </Stack>
           </>
           )}
-        {data ? (
+        {data && data.posts.hasMore ? (
         <Flex>
           <Button
             onClick={() => {
               setVariables({
                 limit: variables.limit,
-                cursor: data.posts[data.posts.length - 1].createdAt,
+                cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
               });
             }}
             isLoading={fetching}
@@ -114,7 +114,7 @@ const Index = () => {
         <Footer>
           <Text>Next ❤️ Chakra</Text>
         </Footer>
-        <CTA />
+        {/* <CTA /> */}
       </Layout>
     </Container>
   );

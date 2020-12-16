4e35a3ef41a552952e8f0b1080a2e7289dbffedb
Peter Williams <pedroeldiablo@yahoo.com>
2020-12-16 14:52:35 +0000

    display creator name and invalidate posts cache when new post created to update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ import {createUrqlClient} from '../utils/createUrqlClient';
 
 const Index = () => {
   const [variables, setVariables] = useState({
-    limit: 33,
+    limit: 15,
     cursor: null as null | string,
   });
 
@@ -57,7 +57,8 @@ const Index = () => {
             <Stack spacing={8}>
               {data!.posts.posts.map((p) => (
                 <Box key={p.id} p={5} shadow="md" borderWidth="1px">
-                  <Heading fontSize="xl">{p.title}</Heading>
+                  <Heading fontSize="xl">{p.title}</Heading>{' '}
+                  <Text>posted by {p.creator.username}</Text>
                   <Text mt={4}>{p.textSnippet}</Text>
                 </Box>
               ))}
